b50a90c33ab10807bbf76e2d3e8c56b2110517fe
sxyazi <sxyazi@gmail.com>
2022-10-19 21:08:22 +0800

    添加固定词、修复八股文语法

--- tools/en_stapling.py
+++ tools/en_stapling.py
@@ -5,13 +5,22 @@ rm = set([
   'pinyin', #拼音
   'leix',   #类型
   'dingy',  #定义
+  'tong',   #同
+  'tongs',  #同时
 
   # 专有名词
   'ip',
   'ios',
 ])
 
+ins = [
+  ('bytes', 'bytes', 16400000),
+]
+ins.sort(key=lambda x: x[1])
+
 buf = StringIO()
+cur = 0
+occur = {}
 with open('easy_en.dict.yaml', 'r') as f:
   for line in f:
     if not line.strip():
@@ -22,6 +31,12 @@ with open('easy_en.dict.yaml', 'r') as f:
     if len(parts) < 2:
       buf.write(line)
       continue
+
+    occur[parts[1]] = True
+    while cur < len(ins) and ins[cur][1] < parts[1]:
+      if ins[cur][1] not in occur:
+        buf.write('%s\t%s\t%d\n' % ins[cur])
+      cur += 1
 
     if parts[1] not in rm:
       buf.write(line)
